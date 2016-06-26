58194db26a4f0f4eb708ab2312cee93c807f396e
Michael <michael.marolt@edu.campus02.at>
2016-06-26 23:19:16 +0200

    Login-UseCase funktional fertiggestellt. Design-Anpassungen folgen.

---
+++ UseCases/src/application/OrdersOverviewController.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class OrdersOverviewController
+{
+	
+}
--- UseCases/src/application/UC1_LoginController.java
+++ UseCases/src/application/UC1_LoginController.java
@@ -1,18 +1,39 @@
 package application;
 
+import java.io.IOException;
 import java.sql.SQLException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class UC1_LoginController
 {
+	@FXML private TextField txtUsername;
+	@FXML private PasswordField txtPassword;
+	@FXML private Label lblErrorMsg;
+	
 	@FXML
-	public void clickLogin() throws SQLException
+	public void clickLogin() throws SQLException, IOException
 	{
-		Boolean loginSuccess = DBConnect.checkUserAndPass("michael", "michi");
+		Boolean loginSuccess = DBConnect.checkUserAndPass(txtUsername.getText(), txtPassword.getText());
 		
 		if (loginSuccess)
-			System.out.println("Success!");
+		{
+			Parent windowOrderOverview = FXMLLoader.load(getClass().getResource("OrdersOverview.fxml"));
+			Scene scene = new Scene(windowOrderOverview);
+			Stage stage = (Stage)txtUsername.getScene().getWindow();
+			stage.setScene(scene);
+			stage.show();
+		}
 		else
-			System.out.println("Failure!");
+		{
+			lblErrorMsg.setVisible(true);
+		}
 	}
 }
