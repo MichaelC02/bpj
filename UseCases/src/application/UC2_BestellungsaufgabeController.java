2b4e60291a30c3b4b89a49dca4479efb86725716
Michael <michael.marolt@edu.campus02.at>
2016-06-26 12:41:35 +0200

    UC2-Klasse hinzugefügt.

---
+++ UseCases/src/application/UC2_BestellungsaufgabeController.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.sql.SQLException;
+import javafx.fxml.FXML;
+
+public class UC2_BestellungsaufgabeController
+{
+	@FXML
+	public void clickLogin() throws SQLException
+	{
+		return;
+	}
+}
