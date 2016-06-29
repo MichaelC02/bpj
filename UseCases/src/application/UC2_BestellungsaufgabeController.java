a712e23b56bd3398d45f545467a6994259850784
mzmollnig <matteo@live.at>
2016-06-29 19:29:05 +0200

    update uc2

--- UseCases/src/application/UC2_BestellungsaufgabeController.java
+++ UseCases/src/application/UC2_BestellungsaufgabeController.java
@@ -2,9 +2,14 @@ package application;
 
 import java.awt.TextField;
 import java.sql.SQLException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class UC2_BestellungsaufgabeController
 {
@@ -16,8 +21,13 @@ public class UC2_BestellungsaufgabeController
 	@FXML private TextField txtarticle_name;
 	@FXML private Label lblErrorMsg;
 	@FXML private TableView<Article> table = new TableView<Article>();
+	private final ObservableList<Article> data = FXCollections.observableArrayList( ) ;
 	
-	
+	String article_name;
+	String article_id;
+	int art_id;
+	String quantity;
+	int quant;
 	
 	@FXML
 	public void clickaddorder() throws SQLException
@@ -30,16 +40,34 @@ public class UC2_BestellungsaufgabeController
 	@FXML
 	public void clickaddarticle() throws SQLException
 	{
-		Article article;
-		Article[] article_list = new Article[50];
+		
+		
+		article_id = txtarticle_id.getText();		
+		article_name = txtarticle_name.getText();	
+		quantity = txtquantity.getText();
+		//Article article;
+		//Article[] article_list = new Article[50];
 		boolean lv_valid;
 		
 		lv_valid = validate();
 		
 		if(lv_valid == true)
 		{
+			quant = Integer.parseInt(quantity);
+			art_id = Integer.parseInt(article_id);
+		//	article = new Article(article_name, art_id, 0, quant);	
+			data.add( new Article(article_name, art_id, 0, quant) );
+		            
+			        
 			
+			
+			TableColumn articleidCol = new TableColumn("Artikelnummer");
+	        articleidCol.setCellValueFactory(new PropertyValueFactory<Article,Integer>("article_id"));
+	        
+	        table.setItems(data);
 		}
+		
+		
 		
 		return;
 	}
@@ -47,12 +75,7 @@ public class UC2_BestellungsaufgabeController
 	
 	public boolean validate( )
 	{
-		String article_id;
-		article_id = txtarticle_id.getText();
-		String article_name;
-		article_name = txtarticle_name.getText();
-		String quantity;
-		quantity = txtquantity.getText();
+		
 		
 		if( article_id == null )
 		{
@@ -60,18 +83,20 @@ public class UC2_BestellungsaufgabeController
 			return false;
 		}
 		
-		if( article_name == null )
+		else if( article_name == null )
 		{
 			lblErrorMsg.setText("Geben Sie einen Artikelnamen ein!");
 			return false;
 		}
 		
-		if( quantity == null )
+		else if( quantity == null )
 		{
 			lblErrorMsg.setText("Geben Sie eine Artikelmenge ein!");
 			return false;
 		}
-		
-		return true; 
+		else
+		{
+			return true; 
+	    }
 	}
 }
